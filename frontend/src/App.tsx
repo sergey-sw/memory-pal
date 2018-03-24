e84f156a39cc047ef69197d2b37819c52939e20f
sergey <sergey.sova42@gmail.com>
2018-03-24 15:21:22 +0300

    create NoteList

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,14 @@
 import * as React from 'react';
 import './App.css';
-
-const logo = require('./logo.svg');
+import NodeList from './note/NoteList';
 
 class App extends React.Component {
 
-  state = {
-     notes: [],
-     isLoading: false
-  };
-
-  componentDidMount() {
-      this.setState({notes: [], isLoading: true});
-
-      fetch('http://localhost:8080/api/notes')
-        .then(response => response.json())
-        .then(data => this.setState({notes: data, isLoading: false}));
-  }
-
   render() {
-    const {notes, isLoading} = this.state;
-
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div>
-          <h2>Note list</h2>
-          {notes.map((note: any) =>
-            <div key={note.id}>
-              {note.text}
-            </div>
-          )}
-        </div>
+        <NodeList />
       </div>
     );
   }
---
+++ frontend/src/note/NoteList.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+class NoteList extends React.Component {
+
+  state = {
+     notes: [],
+     isLoading: false
+  };
+
+  componentDidMount() {
+     this.setState({notes: [], isLoading: true});
+
+     fetch('http://localhost:8080/api/notes')
+          .then(response => response.json())
+          .then(data => this.setState({notes: data, isLoading: false}));
+  }
+
+  render() {
+      const {notes, isLoading} = this.state;
+
+      if (isLoading) {
+        return <p>Loading...</p>;
+      }
+
+      return (
+          <div>
+            <h2>Note list</h2>
+            {notes.map((note: any) =>
+              <div key={note.id}>
+                {note.text}
+              </div>
+            )}
+          </div>
+      );
+    }
+}
+
+export default NoteList;
