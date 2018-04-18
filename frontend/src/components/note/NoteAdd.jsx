6833258c4a502b4b84b40992561a3c4e99282349
sergey <sergey.sova42@gmail.com>
2018-04-18 23:04:07 +0300

    add onFocus / onBlur for note add

--- frontend/src/components/note/NoteAdd.jsx
+++ frontend/src/components/note/NoteAdd.jsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import axios from 'axios';
 
+const DEFAULT_TEXT = 'Enter new note...';
+
 export default class NoteAdd extends React.Component {
 
     constructor(props) {
         super(props);
         this.updateInputValue = this.updateInputValue.bind(this);
+        this.onInputFocus = this.onInputFocus.bind(this);
+        this.onInputBlur = this.onInputBlur.bind(this);
     }
 
     state = {
-        text : 'Enter new note...'
+        text : DEFAULT_TEXT
     }
 
     onNoteAdd = () => {
         axios.post('http://localhost:8080/api/notes/create', { "text" : this.state.text } )
               .then(res => {
                 this.setState({
-                    text : 'Enter new note...'
+                    text : DEFAULT_TEXT
                 });
                 this.props.onAfterSubmit();
               })
@@ -28,6 +32,26 @@ export default class NoteAdd extends React.Component {
         });
     }
 
+    onInputFocus = (evt) => {
+        const input = this.state.text;
+        if (input === DEFAULT_TEXT) {
+           this.setState({
+               text: ''
+           });
+        }
+    }
+
+    onInputBlur = (evt) => {
+        const input = this.state.text;
+        if (input.length === 0) {
+           setTimeout(() => {
+               this.setState({
+                   text: DEFAULT_TEXT
+               });
+           }, 300);
+        }
+    }
+
     render() {
         return (
           <div id="note-add">
@@ -36,6 +60,8 @@ export default class NoteAdd extends React.Component {
                     type="text"
                     value={this.state.text}
                     onChange={evt => this.updateInputValue(evt)}
+                    onFocus={evt => this.onInputFocus(evt)}
+                    onBlur={evt => this.onInputBlur(evt)}
               />
               <button onClick={this.onNoteAdd}>Add</button>
           </div>
